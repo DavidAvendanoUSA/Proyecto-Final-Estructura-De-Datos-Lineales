5eb481c868acdcd229372956fb09f66aff2dacce
NicolasGutierrez2210 <nicolas.gutierrez06@usa.edu.co>
2025-11-20 15:37:15 -0500

    estructura basica

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 conexion = mysql.connector.connect(
-    host= os.getenv("DB_HOST"), 
+    host= os.getenv("DB_HOST"),
     user= os.getenv("DB_USERNAME"),
     password= os.getenv("DB_PASSWORD"),
     database= os.getenv("DB_NAME")
