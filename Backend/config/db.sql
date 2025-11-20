40f7df731903f0d777c7a8fa690fbbf1f4684a15
NicolasGutierrez2210 <nicolas.gutierrez06@usa.edu.co>
2025-11-20 16:07:36 -0500

    subir DB para hacer la migracion(crearla)

---
+++ Backend/config/db.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS sistema_colas;
+USE sistema_colas;
+
+CREATE TABLE corridas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tiempo DATETIME NOT NULL
+);
+
+CREATE TABLE colas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    corrida_id INT NOT NULL,
+    nombre_id VARCHAR(1) NOT NULL,
+    n_entrada INT,
+    n_atendidos INT,
+    n_no_atendidos INT,
+    FOREIGN KEY (corrida_id) REFERENCES corridas(id)
+);
